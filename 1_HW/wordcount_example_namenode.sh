bd7ac833027163e6d735a4c0193a4d2ff74d6782
Zanabazer <aroberge66@gmail.com>
2023-02-16 17:05:21 -0500

    instructions for running the wordcount example

---
+++ 1_HW/wordcount_example_namenode.sh
@@ -0,0 +1,12 @@
+#Format the file System
+hdfs namenode -format
+#Start everything
+start-all.sh
+# Make a directory in hfds
+# Run wordcount <in> <outdir> 
+
+hadoop fs -mkdir /tmpdir
+hadoop fs -put to_count /tmpdir
+
+hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.4.jar wordcount /tmpdir/to_count /tmpdir/out
+
