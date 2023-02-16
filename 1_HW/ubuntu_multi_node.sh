857eea712a402e7f9bcf11fb3d0cdc00975c136d
Zanabazer <aroberge66@gmail.com>
2023-02-16 15:27:06 -0500

    added sleeps for the ssh keygens to occur

--- 1_HW/ubuntu_multi_node.sh
+++ 1_HW/ubuntu_multi_node.sh
@@ -47,11 +47,22 @@ fi
 echo "Executing Key Generation"
 cmd="#!/bin/bash"
 echo $cmd >> ssh_master.sh
+cmd="if test -f ~/.ssh/id_rsa.pub; then"
+echo $cmd >> ssh_master.sh
+cmd="mv ~/.ssh/backup_keys ~/.ssh/authorized_keys"
+echo $cmd >> ssh_master.sh
+cmd="rm ~/.ssh/id_rsa*"
+echo $cmd >> ssh_master.sh
+cmd="fi"
+echo $cmd >> ssh_master.sh
+cmd="cp ~/.ssh/authorized_keys ~/.ssh/backup_keys"
+echo $cmd >> ssh_master.sh
 cmd="ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1"
 echo $cmd >> ssh_master.sh
 cmd="cat .ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
 echo $cmd >> ssh_master.sh
 echo "echo 'ssh -o StrictHostKeyChecking=no -t $user_str@$server_str${node_id_ary[0]}$suffix_str'" >> ssh_master.sh;
+
 echo '$SHELL' >> ssh_master.sh;
 chmod +x ssh_master.sh;
 if test -f exec.sh; then
@@ -60,27 +71,30 @@ fi
 echo "ssh -o StrictHostKeyChecking=no -t $user_str@$server_str${node_id_ary[0]}$suffix_str < $directory/ssh_master.sh;" >> exec.sh; # & to run in background
 echo '$SHELL' >> exec.sh
 chmod +x exec.sh;
-$shell_cmd "$directory/exec.sh" &
+$shell_cmd "$directory/exec.sh"
 
+# Sleeps are needed to wait for the scp to occur and keygen to happen
+sleep 2
 echo "Executing SCP Copy"
 $shell_cmd "scp $user_str@$server_str${node_id_ary[0]}$suffix_str:.ssh/authorized_keys tmp_keys"
+sleep 2
 $shell_cmd "scp tmp_keys $user_str@$server_str${node_id_ary[1]}$suffix_str:.ssh/authorized_keys"
 $shell_cmd "scp tmp_keys $user_str@$server_str${node_id_ary[2]}$suffix_str:.ssh/authorized_keys"
 $shell_cmd "scp tmp_keys $user_str@$server_str${node_id_ary[3]}$suffix_str:.ssh/authorized_keys"
 
 
-for node_id in ${node_id_ary[@]}; do
-    echo " ******** processing commands for node $node_id ******** ";
-    echo "# $current_date" > $node_id.sh;
-    final_cmd_str="$cmd_str$user_str@$server_str$node_id$suffix_str$script_str";
-    echo -e "generated:    $final_cmd_str \n";
-    echo "$final_cmd_str;" >> $node_id.sh;
-    echo "echo results from node $node_id;" >> $node_id.sh;
-    echo "echo ssh -o StrictHostKeyChecking=no -t $user_str@$server_str$node_id$suffix_str;" >> $node_id.sh;
-    echo '$SHELL' >> $node_id.sh;
-    chmod +x $node_id.sh;
-    $shell_cmd "$directory/$node_id.sh" & # & with no ; to run in background
-done
+# for node_id in ${node_id_ary[@]}; do
+#     echo " ******** processing commands for node $node_id ******** ";
+#     echo "# $current_date" > $node_id.sh;
+#     final_cmd_str="$cmd_str$user_str@$server_str$node_id$suffix_str$script_str";
+#     echo -e "generated:    $final_cmd_str \n";
+#     echo "$final_cmd_str;" >> $node_id.sh;
+#     echo "echo results from node $node_id;" >> $node_id.sh;
+#     echo "echo ssh -o StrictHostKeyChecking=no -t $user_str@$server_str$node_id$suffix_str;" >> $node_id.sh;
+#     echo '$SHELL' >> $node_id.sh;
+#     chmod +x $node_id.sh;
+#     $shell_cmd "$directory/$node_id.sh" & # & with no ; to run in background
+# done
 
 # only on NameNode
 #hdfs namenode -format
