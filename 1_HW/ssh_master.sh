c5d14ca4aca7b19f116eaa025a5af0abc955874b
Zanabazer <aroberge66@gmail.com>
2023-02-16 10:37:04 -0500

    updated gitignore to not ignore auto-generated files; removed ssh_master autogen file

--- 1_HW/ssh_master.sh
+++
@@ -1,4 +0,0 @@
-ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa -y
-cat .ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-echo ssh -o StrictHostKeyChecking=no -t aroberge@node0.hw1node4.dic-uml-s23-pg0.wisc.cloudlab.us;
-$SHELL
