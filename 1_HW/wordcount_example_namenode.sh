0aa816d41bab3ec503244a947c58611d4ea205d9
Zanabazer <aroberge66@gmail.com>
2023-02-18 15:38:39 -0500

    chagned line endings added chapter for wordcount example

--- 1_HW/wordcount_example_namenode.sh
+++ 1_HW/wordcount_example_namenode.sh
@@ -1,4 +1,19 @@
 # ssh -o StrictHostKeyChecking=no -t LutzD00D@apt099.apt.emulab.net < wordcount_example_namenode.sh
+config_file="multi_config.sh";
+directory=$(pwd);
+cmd_str="ssh -o StrictHostKeyChecking=no -t "; # -o StrictHostKeyChecking no
+script_str=" < ubuntu_single_node.sh";
+current_date=$(date);
+
+if test -f $config_file;
+    then echo " **** Config File Exists, source it. **** ";
+    source ./$config_file;
+else
+    echo " **** Config File Template Copied **** ";
+    echo " **** ENTER USER VALUES INTO MULTI LOCAL $config_file **** ";
+    cp multi_config_template.sh $config_file;
+    exit
+fi
 
 echo " **** Performing HDFS format **** ";
 hdfs namenode -format;
@@ -7,7 +22,8 @@ echo " **** start-all **** ";
 start-all.sh
 
 # echo " **** make tmp and to_count dir **** ";
-hadoop fs -mkdir -p /tmp/to_count
-
+hadoop fs -mkdir -p /tmp/
+scp around_the_world.txt $user_str@$server_str$ext_node_id_ary[0]$suffix_str:~/around_the_world.txt
+hadoop fs -put /tmp/around_the_world.txt
 # echo " ****  Run wordcount <in> <outdir> **** "; 
-hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.4.jar wordcount /tmpdir/to_count /tmpdir/out
+hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.4.jar wordcount /tmpdir/around_the_world.txt /tmpdir/out
